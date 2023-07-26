6a12eaf683c9edcac14d09983bfffb08abc970fd
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-07-26 09:18:09 -0300

    Regras de negocio

--- fatorial.java
+++ fatorial.java
@@ -4,8 +4,20 @@ import java.util.Scanner;
 public class fatorial {
 	
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
 		
-	}
+		int fat = 1;
+		for (int i=1; i<=n; i++) {
+			fat = fat * i;
+		}
+		
+		System.out.println(fat);
+		
+		sc.close();
+			}
+
 
 }
